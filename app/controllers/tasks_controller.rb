c9cc13f4d5db247d6c2fe9521dad145b19a2112c
mrk19960617 Lienhard <mark.lienhard@kellogg.northwestern.edu>
2025-02-25 21:59:21 +0000

    Completing Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :require_login, only: [:create, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -6,13 +8,31 @@ class TasksController < ApplicationController
   def create
     @task = Task.new
     @task["description"] = params["description"]
-    @task.save
+    if @task.save
+      flash[:notice] = "Task added successfully."
+    else
+      flash[:alert] = "Error adding task."
+    end
     redirect_to "/tasks"
   end
 
   def destroy
-    @task = Task.find_by({ "id" => params["id"] })
-    @task.destroy
+    @task = Task.find_by(id: params["id"])
+    if @task
+      @task.destroy
+      flash[:notice] = "Task completed!"
+    else
+      flash[:alert] = "Task not found."
+    end
     redirect_to "/tasks"
+  end
+
+  private
+
+  def require_login
+    unless session["user_id"]
+      flash[:alert] = "You must be logged in to create or delete tasks."
+      redirect_to "/login"
+    end
   end
 end
