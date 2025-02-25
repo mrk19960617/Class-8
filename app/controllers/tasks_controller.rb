d4f1a4371a0637499017a9f58cac5396809fa3b2
mrk19960617 Lienhard <mark.lienhard@kellogg.northwestern.edu>
2025-02-25 22:13:59 +0000

    Assigning tasks to users

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,15 @@
 class TasksController < ApplicationController
-  before_action :require_login, only: [:create, :destroy]
+  before_action :require_login
 
   def index
-    @tasks = Task.all
+    @tasks = Task.where(user_id: session["user_id"]) # Show only logged-in user's tasks
   end
 
   def create
     @task = Task.new
-    @task["description"] = params["description"]
+    @task.description = params["description"]
+    @task.user_id = session["user_id"] # Assign task to logged-in user
+
     if @task.save
       flash[:notice] = "Task added successfully."
     else
@@ -17,12 +19,13 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find_by(id: params["id"])
+    @task = Task.find_by(id: params["id"], user_id: session["user_id"]) # Ensure user can only delete their own task
+
     if @task
       @task.destroy
       flash[:notice] = "Task completed!"
     else
-      flash[:alert] = "Task not found."
+      flash[:alert] = "Task not found or unauthorized."
     end
     redirect_to "/tasks"
   end
@@ -31,7 +34,7 @@ class TasksController < ApplicationController
 
   def require_login
     unless session["user_id"]
-      flash[:alert] = "You must be logged in to create or delete tasks."
+      flash[:alert] = "You must be logged in to view, create, or delete tasks."
       redirect_to "/login"
     end
   end
